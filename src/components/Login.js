8cc90554d93a7296fe14573e8af4c90badf0c7a6
Christian Medina <christianmedina121@hotmail.com>
2023-03-03 06:56:16 +0000

    Installed MUI core library. Added AccountMenu component. Removed extraneous items for navbar. Updated README.md

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,29 @@
 import './Login.css';
 
-function Login() {
-    return (
-        <div>
-            <h1>Login Page</h1>
-            <form>
-              <label>
-                Email:
-                <input type="text" name="name" />
-              </label>
-              <label>
-                Password:
-                <input type="text" name="name" />
-              </label>
-              <input type="submit" value="Login" />
-            </form>
-        </div>);
+
+
+function Login(props) {
+  
+  function handleSubmit(){
+    //TODO: Call server to check if user exists and retreive user
+    props.authenticated();
+  }
+  
+  return (
+      <div>
+          <h1>Login Page</h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Email:
+              <input type="text" name="name" />
+            </label>
+            <label>
+              Password:
+              <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Login" />
+          </form>
+      </div>);
 }
 
 export default Login;
