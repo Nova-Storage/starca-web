dac329885a2a2724175402dd4ad998b402e0c3e1
Mohamed Rachid <mohamednrachid@gmail.com>
2023-03-05 17:44:20 -0500

    Testing working with the map
    Got the map to display

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,17 +1,48 @@
 import './SearchResults.css';
+import React from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-const mapStyles = {
-    width: '100%',
-    height: '100%'
+  const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+  
+  const center = {
+    lat: -3.745,
+    lng: -38.523
   };
   
 function SearchResults() {
-    return (
-        <>
-            <h2>Search Result</h2>
-        </>
-    );
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyBRoANqiQxK94SQeSr0W2eQoLbnkOH0dTA"
+      })
+      const [map, setMap] = React.useState(null)
+
+      const onLoad = React.useCallback(function callback(map) {
+        // This is just an example of getting and using the map instance!!! don't just blindly copy!
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+    
+        setMap(map)
+      }, [])
+    
+      const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+      }, [])
+    
+      return isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <></>
+          </GoogleMap>
+      ) : <></>
 }
 
-export default SearchResults;
+export default React.memo(SearchResults);
