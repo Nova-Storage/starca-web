bfc2d71c2066deaf04bc7492d7162fd36fd80c92
Mohamed Rachid <mohamednrachid@gmail.com>
2023-03-21 15:11:36 -0400

    border outline progress update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,6 @@ import './Dashboard.css';
 import React, {useState, useEffect} from 'react'
 import Map from './Map.js'
 import { Switch } from '@mui/material';
-import Autocomplete from 'react-google-autocomplete';
 import Geocode from 'react-geocode'
 
 Geocode.setApiKey(`${process.env.REACT_APP_MAPS_API_KEY}`)
@@ -12,41 +11,6 @@ function Dashboard() {
 
   // State Handler for the center and the Google Map
   const [showMap, setShowMap] = useState(true)
-  const [mapCenter, setCenter] = useState({
-    lat: 40.7484,
-    lng: -73.9857
-  })
-
-  // Function to get the Lat and Lng from an address.
-  function getCoords(place) {
-    console.log(place.place_id)
-    Geocode.fromAddress(place.formatted_address).then( (response) => {
-      setCenter({
-        lat: response.results[0].geometry.location.lat,
-        lng: response.results[0].geometry.location.lng
-      })
-    },
-    (error) => {
-      console.log(error)
-    })
-  }
-
-  // Search Box Autocomplete options
-  const searchOptions = {
-    types: ["(regions)"],
-    componentRestrictions: { country: "us" },
-  }
-
-  // Hook that updates the map when center is updated.
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-
-      setCenter({
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      })
-    })
-    }, [])
 
   // Toggle whether the map is visible.
   const toggleMap = () => {
@@ -70,12 +34,13 @@ function Dashboard() {
           checked={showMap}
           onChange={toggleMap}
         />
-        <Autocomplete   
+        {/* <Autocomplete   
           apiKey={`${process.env.REACT_APP_MAP_ID}`}
           options={searchOptions}
           onPlaceSelected={(place) => {setCenter(getCoords(place))}}> 
-        </Autocomplete>
-          <Map center={mapCenter}/>
+        </Autocomplete> */}
+          {/* <Map center={mapCenter}/> */}
+          <Map />
       </div>
       )
     }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,42 +1,109 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, {
+  useState,
+  useEffect
+} from 'react'
+import { GoogleMap, useJsApiLoader, Polygon } from '@react-google-maps/api';
 import { CircularProgress } from '@mui/material';
+import Autocomplete from 'react-google-autocomplete';
+import Geocode from 'react-geocode'
+import { initializeApp } from 'firebase/app'
 
-  // Google Map API Libraries to be included. Defined here for performance reasons
-  const lib = ['places']
 
-  // Styling for the map
-  const containerStyle = {
-    width: '40vw',
-    height: '89vh',
-    position: 'fixed',
-    overflow: 'hidden',
-    marginTop: '1%',
-    borderRadius: '10px',
-    marginLeft: '2vmin',
-    border: '1px solid black'
-  };
+// Geocode library initialization
+Geocode.setApiKey(`${process.env.REACT_APP_MAPS_API_KEY}`)
+Geocode.setLocationType("ROOFTOP");
 
-  const US_BOUNDS = {
-    north: 49.382808,
-    south: 24.521208,
-    west: -124.736342,
-    east: -66.945392
+// Google Map API Libraries to be included. Defined here for performance reasons
+const lib = ['places']
+
+// Styling for the map
+const containerStyle = {
+  width: '40vw',
+  height: '89vh',
+  position: 'fixed',
+  overflow: 'hidden',
+  marginTop: '1%',
+  borderRadius: '10px',
+  marginLeft: '2vmin',
+  border: '1px solid black'
+};
+
+// Search Box Autocomplete options
+const searchOptions = {
+  types: ["locality", "postal_code"],
+  componentRestrictions: { country: "us" },
+}
+
+// Map Options. Disable various UI elements (PoI, map types, etc.)
+const mapOptions = {
+  mapId: `${process.env.REACT_APP_MAP_ID}`,
+  streetViewControl: false,
+  mapTypeControl:false,
+  fullscreenControl: false,
+  feature: {
+    placeId: "ChIJuXAqFCUAw4kRNDvR1irBWyc"
   }
+}
 
-  // Map Options. Disable various UI elements (PoI, map types, etc.)
-  const mapOptions = {
-    mapId: `${process.env.REACT_APP_MAP_ID}`,
-    streetViewControl: false,
-    mapTypeControl:false,
-    fullscreenControl: false,
-    feature: {
-      placeidId: "ChIJuXAqFCUAw4kRNDvR1irBWyc"
-    }
-  }
+let test = require(`./stateZIPs/new_jersey.json`)
+const test2 = test.features.find(element => element.properties.ZCTA5CE10 === '07102')
+
+const paths = []
+
+for (var i = 0; i < test2.geometry.coordinates[0].length; i++) {
+  paths.push({
+    lat: test2.geometry.coordinates[0][i][1],
+    lng: test2.geometry.coordinates[0][i][0]
+  })
+}
+
+console.log(paths)
+
+const lineOptions = {
+  fillColor: "lightblue",
+  fillOpacity: 0.4,
+  strokeColor: "white",
+  strokeOpacity: 1,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  geodesic: false,
+  zIndex: 1
+}
 
 // Map component. Takes a center location as a prop. Displays a Google Map centered at 'center'
-function Map({center}) {
+// function Map({center}) {
+function Map() {
+
+  const [mapCenter, setCenter] = useState({
+    lat: 40.7484,
+    lng: -73.9857
+  })
+
+  // Function to get the latitude and longitude of a place and recenter the map to that location.
+  function getCoords(place) {
+    Geocode.fromAddress(place.formatted_address).then( (response) => {
+      setCenter({
+        lat: response.results[0].geometry.location.lat,
+        lng: response.results[0].geometry.location.lng
+      })
+    },
+    (error) => {
+      console.log(error)
+    })
+  }
+  
+  // Hook that updates the map when center is updated.
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+  
+      setCenter({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      })
+    })
+    }, [])
 
   // Load the Google Map using the useJsApiLoader hook
   const {isLoaded} = useJsApiLoader({
@@ -53,15 +120,26 @@ function Map({center}) {
   return (
 
     <div className='map'>
+        <Autocomplete   
+          apiKey={`${process.env.REACT_APP_MAP_ID}`}
+          options={searchOptions}
+          onPlaceSelected={(place) => {
+            setCenter(getCoords(place))}
+            }> 
+        </Autocomplete>
       <GoogleMap 
-        center={center}
-        restrictions={{
-          latlngBounds: US_BOUNDS,
-          strictBounds: true
-        }}
+        center={mapCenter}
         zoom={13} 
         mapContainerStyle={containerStyle}
-        options={mapOptions}>
+        options={mapOptions}
+        version="beta"
+        // onCenterChanged={() => console.log("TEST")}
+        >
+                    <Polygon
+            paths={paths}
+            options={lineOptions}
+            visible={true}
+          />
       </GoogleMap>
     </div>
   )
