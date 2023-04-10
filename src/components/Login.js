2a695d377edab9d8f825f058e488f489a973ea46
Christian Medina <christianmedina121@hotmail.com>
2023-04-10 00:23:18 -0400

    Modifying react-slideshow

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,6 +11,9 @@ import IconButton from '@mui/material/IconButton';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import starcaLogo from '../images/starca-logo-icon.png';
+import 'react-slideshow-image/dist/styles.css'
+
+import { Fade } from 'react-slideshow-image';
 
 function Login(props) {
 
@@ -117,9 +120,31 @@ function Login(props) {
           <p>Don't have an account yet? <a className="register-link" onClick={handleSignUp}>Sign up</a></p>
           </div>
           <div>
-            Carousel stuff
-          </div>
+            <Fade>
+              {fadeImages.map((fadeImage, index) => (
+                <div key={index} className='each-slide'>
+                  <img style={{ width: '100%', height: "90vh" }} src={fadeImage.url} />
+                  <h2>{fadeImage.caption}</h2>
+                </div>
+              ))}
+            </Fade>
+            </div>
       </div>);
 }
 
 export default Login;
+
+const fadeImages = [
+  {
+    url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    caption: 'First Slide'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+    caption: 'Second Slide'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    caption: 'Third Slide'
+  },
+];
