c780919f6d0c5c6f77fdd6d3246aa30173950a4b
Christian Medina <christianmedina121@hotmail.com>
2023-04-24 23:39:26 -0400

    Fixed issue where retrieving 0 listings crashes the website. Fixed issue where loggin out crashes the server.

--- src/components/MyListings.js
+++ src/components/MyListings.js
@@ -2,9 +2,7 @@ import React, { useEffect } from "react";
 import "./MyListings.css";
 import ItemListing from './ItemListing.js';
 import { StyledButton } from './StyledMuiComponents';
-import {useNavigate} from 'react-router-dom';
-import { useEffect } from "react";
-import { useLocation } from 'react-router-dom'
+import {useNavigate, useLocation} from 'react-router-dom';
 import { Snackbar, Alert  } from '@mui/material'
 
 
@@ -68,41 +66,36 @@ const MyListings = (props) => {
                       } }
     */
 
-
-    //TODO: Create "MyListing item"
+    //TODO: Create "MyListing item component"
 
     return (
       <div className="my-listings-container">
+        <Snackbar 
+            open={showSnackbar}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            autoHideDuration={3000}>
+            <Alert severity='success'>{snackbarMessage}</Alert>
+        </Snackbar>
         <StyledButton type="submit" 
                       variant="contained" 
                       className="create-listing-button" 
-      <Snackbar 
-            open={showSnackbar}
-            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-            autoHideDuration={3000}
-        >
-            <Alert severity='success'>{snackbarMessage}</Alert>
-        </Snackbar>
                       style={{width: '10rem'}}
                       onClick={handleNewListingClick}>New Listing</StyledButton>
         <h1 className="my-listings-title" id="title">My Listings</h1>
         <div className="my-listings-grid-container">
             {myListings.map(listing => {
             return(
-                
-                    <ItemListing 
-                        listingImages = { listing.imageUrls ? listing.imageUrls : false }
-                        listingTitle={ listing.ltitle }
-                        listingDescription={ listing.ldescr }
-                        listingPrice={ listing.lprice }
-                        listingAddress={ listing.lstreet }
-                        listingCity={ listing.lcity }
-                        listingState={ listing.lstate } 
-                        listingZip={ listing.lzip }
-                        listingAmenities= { ['Security Cameras, ', 'Biometrics, ', 'Wheelchair Accessible'] }/>
-                
-                );
-            
+                <ItemListing 
+                    listingImages = { listing.imageUrls ? listing.imageUrls : false }
+                    listingTitle={ listing.ltitle }
+                    listingDescription={ listing.ldescr }
+                    listingPrice={ listing.lprice }
+                    listingAddress={ listing.lstreet }
+                    listingCity={ listing.lcity }
+                    listingState={ listing.lstate } 
+                    listingZip={ listing.lzip }
+                    listingAmenities= { ['Security Cameras, ', 'Biometrics, ', 'Wheelchair Accessible'] }/>
+            );
             })}
         </div>
         <h1 className="my-listings-title" id="rentals-title">My Rentals</h1>
